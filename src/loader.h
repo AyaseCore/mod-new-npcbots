9029cd6d22081679df8c266ba083e23115817340
ayase <137056643@qq.com>
2020-03-18 22:58:32 +0800

    loader.h

--- src/loader.h
+++ src/loader.h
@@ -1,1 +1,62 @@
-void AddMyPlayerScripts();
+#ifdef NPCBOT  
+void AddSC_death_knight_bot();
+void AddSC_druid_bot();
+void AddSC_hunter_bot();
+void AddSC_mage_bot();
+void AddSC_paladin_bot();
+void AddSC_priest_bot();
+void AddSC_rogue_bot();
+void AddSC_shaman_bot();
+void AddSC_warlock_bot();
+void AddSC_warrior_bot();
+void AddSC_blademaster_bot();
+void AddSC_sphynx_bot();
+void AddSC_archmage_bot();
+void AddSC_dreadlord_bot();
+void AddSC_spellbreaker_bot();
+void AddSC_dark_ranger_bot();
+void AddSC_archmage_bot_pets();
+void AddSC_dark_ranger_bot_pets();
+void AddSC_dreadlord_bot_pets();
+void AddSC_hunter_bot_pets();
+void AddSC_warlock_bot_pets();
+void AddSC_deathknight_bot_pets();
+void AddSC_priest_bot_pets();
+void AddSC_shaman_bot_pets();
+void AddSC_mage_bot_pets();
+void AddSC_druid_bot_pets();
+void AddSC_script_bot_commands();
+#endif
+
+#ifdef NPCBOT
+void AddNpcbotScripts()
+{
+    AddSC_death_knight_bot();
+    AddSC_druid_bot();
+    AddSC_hunter_bot();
+    AddSC_mage_bot();
+    AddSC_paladin_bot();
+    AddSC_priest_bot();
+    AddSC_rogue_bot();
+    AddSC_shaman_bot();
+    AddSC_warlock_bot();
+    AddSC_warrior_bot();
+    AddSC_blademaster_bot();
+    AddSC_sphynx_bot();
+    AddSC_archmage_bot();
+    AddSC_dreadlord_bot();
+    AddSC_spellbreaker_bot();
+    AddSC_dark_ranger_bot();
+    AddSC_archmage_bot_pets();
+    AddSC_dark_ranger_bot_pets();
+    AddSC_dreadlord_bot_pets();
+    AddSC_hunter_bot_pets();
+    AddSC_warlock_bot_pets();
+    AddSC_deathknight_bot_pets();
+    AddSC_priest_bot_pets();
+    AddSC_shaman_bot_pets();
+    AddSC_mage_bot_pets();
+    AddSC_druid_bot_pets();
+    AddSC_script_bot_commands();
+}
+#endif // NPCBOT
