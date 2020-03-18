69296cdb6f21bd017d79029a979f0b51f6574e60
ayase <137056643@qq.com>
2020-03-18 22:58:02 +0800

    配置cmake

---
+++ cmake/after_load_cmake_modules.cmake
@@ -0,0 +1,6 @@
+AC_ADD_SCRIPT_LOADER("Npcbot" "${CMAKE_MOD_NEW_NPCBOTS_DIR}/src/loader.h")
+
+CU_ADD_HOOK(BEFORE_GAME_LIBRARY "${CMAKE_MOD_NEW_NPCBOTS_DIR}/cmake/before_game_lib.cmake")
+CU_ADD_HOOK(BEFORE_SCRIPTS_LIBRARY "${CMAKE_MOD_NEW_NPCBOTS_DIR}/cmake/before_scripts_lib.cmake")
+
+AC_ADD_CONFIG_FILE("${CMAKE_MOD_NEW_NPCBOTS_DIR}/conf/newnpcbots.conf.dist")
---
+++ cmake/before_game_lib.cmake
@@ -0,0 +1,13 @@
+CollectSourceFiles(
+  ${CMAKE_MOD_NEW_NPCBOTS_DIR}/src/npcbots/
+  PRIVATE_SOURCES)
+  
+file(GLOB_RECURSE sources ${CMAKE_MOD_NEW_NPCBOTS_DIR}/src/npcbots/*)
+source_group("AI\\Npcbots" FILES ${sources})
+
+add_definitions(-DNPCBOT)
+
+CollectIncludeDirectories(
+  ${CMAKE_MOD_NEW_NPCBOTS_DIR}
+  PUBLIC_INCLUDES)
+
